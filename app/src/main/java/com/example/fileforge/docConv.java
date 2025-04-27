ebf2a8b5749976ba9d2266d994854e61273fb8db
CodeSmithAditya <adadityadas99@gmail.com>
2025-04-27 15:32:39 +0530

    Update

--- app/src/main/java/com/example/fileforge/docConv.java
+++ app/src/main/java/com/example/fileforge/docConv.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class wordConv extends AppCompatActivity {
+public class docConv extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_word_conv);
+        setContentView(R.layout.activity_doc_conv);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
